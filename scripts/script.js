5c308fdcb48a5204f9d13da0e978c32113a214c1
glyvin <glyvin@pointcad1.onmicrosoft.com>
2022-04-04 14:56:29 +0300

    done

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+
+let popupName =  document.querySelector('.popup__input-name');
+let popupAbout =  document.querySelector('.popup__input-about');
+
+let profileName =  document.querySelector('.profile__name');
+let profileAbout =  document.querySelector('.profile__caption');
+
+
+
+profileEditButton.addEventListener('click', function() {
+    popup.classList.add('popup_opened');
+    popupName.value = profileName.textContent;
+    popupAbout.value = profileAbout.textContent;
+});
+
+let closeButton = document.querySelector('.popup__close-button');
+
+closeButton.addEventListener('click', function() {
+    popup.classList.remove('popup_opened');
+});
+
+let saveButton = document.querySelector('.popup__save-button');
+
+saveButton.addEventListener('click', function() {
+    profileName.innerHTML = popupName.value;
+    console.log(profileName.innerHTML);
+    profileAbout.innerHTML = popupAbout.value;
+    popup.classList.remove('popup_opened');
+});
