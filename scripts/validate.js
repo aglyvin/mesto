9950a05f76a2f4bde27a3f261b051a9e1a490962
aglyvin <alexej@glyvn.com>
2022-04-24 21:25:09 +0300

    cjanges

---
+++ scripts/validate.js
@@ -0,0 +1,46 @@
+function enableValidation(config) {
+    const form = document.querySelector(config.formSelector);
+    const inputs = form.querySelectorAll(config.inputSelector); 
+
+    inputs.forEach(element => {
+        element.addEventListener('input', (event) => handleFormInput(event, form, config));
+    });
+
+    form.addEventListener('submit' , event => handleFormSubmit(event));
+    toggleButton(form, config);
+}
+
+function toggleButton(form, config) {
+    const button = form.querySelector(config.buttonSelector);
+    button.disabled = !form.checkValidity();
+    button.classList.toggle('popup__save-button_disabled', !form.checkValidity());
+
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+}
+
+function handleFormInput(event, form, config) {
+    const input = event.target;
+    const errorNode = document.querySelector(`#${input.id}-error`);
+    if (input.validity.valid) {
+        errorNode.textContent = '';
+    } else {
+        errorNode.textContent = input.validationMessage;
+    }
+    toggleButton(form, config);
+
+}
+
+enableValidation({
+    formSelector: '.popup__form[name=edit-profile]',
+    inputSelector: '.popup__input',
+    buttonSelector: '.popup__save-button'
+});
+
+enableValidation({
+    formSelector: '.popup__form[name=add-place]',
+    inputSelector: '.popup__input',
+    buttonSelector: '.popup__save-button'
+});
