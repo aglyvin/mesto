e85c6353bc497929d8e6390a7f87996b781b7474
glyvin <glyvin@pointcad1.onmicrosoft.com>
2022-06-22 18:51:02 +0300

    PR9. Changes after 2-nd review

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+export const validationConfig = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    buttonSelector: '.popup__save-button',
+    inactiveButtonClass: 'popup__save-button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+};
+export const profileEditButton = document.querySelector('.profile__edit-button');
+export const changeAvatarButton = document.querySelector('.profile__avatar');
+export const cardsContainerSelector = '.elements';
+export const previewSelector = '.popup-preview';
+export const editProfileSelector = '.popup-edit-profile';
+export const profileNameSelector = '.profile__name';
+export const profileCaptionSelector = '.profile__caption';
+export const profileAvatarSelector = '.profile__avatar';
+export const changeAvatarSelector = '.popup-change-avatar';
+export const popupAddPhotoSelector = '.popup-add-photo';
+export const addButtonSelector = '.profile__add-button';
+export const popupDeleteCardSelector = '.popup-delete-card';
